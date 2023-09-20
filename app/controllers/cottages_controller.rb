4538451b25508b61dcebec27c123786b6ae3f4aa
Ellen CHan <ellenycc@gmail.com>
2023-09-20 14:58:59 +0100

    Add seeds, edit index and show page

--- app/controllers/cottages_controller.rb
+++ app/controllers/cottages_controller.rb
@@ -1,2 +1,9 @@
 class CottagesController < ApplicationController
+  def index
+    @cottages = Cottage.all
+  end
+
+  def show
+    @cottage = Cottage.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Cottage.destroy_all
+
+Cottage.create!([
+  {
+    name: 'Charming Cottage in Cornwall with Garden & Jacuzzi',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    price_per_night: 90,
+    number_of_guests: 6
+  },
+  {
+    name: 'Lovely Cottage in Hemswell with Private Parking',
+    address: 'Hemswell',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    price_per_night: 75,
+    number_of_guests: 5
+  },
+  {
+    name: 'Wonderful Cottage in Bampton with Barbecue & Garden',
+    address: 'Bampton',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    price_per_night: 75,
+    number_of_guests: 5
+  },
+  {
+    name: 'Nice Lodge in Great Busby with Sauna',
+    address: 'Great Busby, England',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    price_per_night: 83,
+    number_of_guests: 4
+  }])
