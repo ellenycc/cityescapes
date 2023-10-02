9cc1f09b9eb00b4f4970522c29a6eaf68edfe308
Ellen CHan <ellenycc@gmail.com>
2023-10-02 21:12:38 +0100

    edit index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "cottages#index"
   resources :cottages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
