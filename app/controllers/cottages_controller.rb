7fad1b7da0baae918391f619f4bb3819854a0b13
Ellen CHan <ellenycc@gmail.com>
2023-10-02 20:02:51 +0100

    add form

--- app/controllers/cottages_controller.rb
+++ app/controllers/cottages_controller.rb
@@ -6,4 +6,20 @@ class CottagesController < ApplicationController
   def show
     @cottage = Cottage.find(params[:id])
   end
+
+  def new
+    @cottage = Cottage.new
+  end
+
+  def create
+    @cottage = Cottage.new(cottage_params)
+    @cottage.save
+    redirect_to cottage_path(@cottage)
+  end
+
+  private
+
+  def cottage_params
+    params.require(:cottage).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
